e68ef0863edf76cdd46d281266544201ed8cc7ed
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2021-10-22 02:30:22 +0800

    can do sound, wait for random

--- recordMain.py
+++ recordMain.py
@@ -1,58 +1,83 @@
+import datetime
 import cv2
+import pandas
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool, ColumnDataSource
 from playsound import playsound
 import threading
 import time
+import pyaudio
 import wave
+
+threadList = []
 
 
 def OHOHOHOHO():
-    print("MOVING")
-    info = wave.open('output.wav', 'r')
-    frames = info.getnframes()
-    rate = info.getframerate()
-    duration = frames / float(rate)
-    playsound("./output.wav")
-    print("the duar is :", duration)
-    time.sleep(duration)
+    chunk = 1024
+    # open a wav format music
+    f = wave.open(r"./output.wav", "rb")
+    # instantiate PyAudio
+    p = pyaudio.PyAudio()
+    # open stream
+    stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
+                    channels=f.getnchannels(),
+                    rate=f.getframerate(),
+                    output=True)
+    # read data
+    data = f.readframes(chunk)
+
+    # play stream
+    while data:
+        stream.write(data)
+        data = f.readframes(chunk)
+
+    # stop stream
+    stream.stop_stream()
+    stream.close()
+
+    # close PyAudio
+    p.terminate()
+    threadList.clear()
 
 
-first_frame = None
-status_list = [None, None]
-times = []
-video = cv2.VideoCapture(0)
+def main():
+    first_frame = None
+    status_list = [None, None]
+    times = []
+    df = pandas.DataFrame(columns=["Start", "End"])
+    video = cv2.VideoCapture(0)
 
-time.sleep(10)
-print("startingq")
-t = threading.Thread(target=OHOHOHOHO)
+    a = 1
+    while True:
+        a = a + 1
+        check, frame = video.read()
+        status = 0
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-while True:
-    check, frame = video.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        if first_frame is None:
+            first_frame = gray
+            continue
 
-    if first_frame is None:
-        first_frame = gray
-        continue
+        delta_frame = cv2.absdiff(first_frame, gray)
+        th_delta = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+        th_delta = cv2.dilate(th_delta, None, iterations=0)
+        (cnts, _) = cv2.findContours(th_delta.copy(),
+                                     cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    delta_frame = cv2.absdiff(first_frame, gray)
-    th_delta = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
-    th_delta = cv2.dilate(th_delta, None, iterations=0)
-    (cnts, _) = cv2.findContours(th_delta.copy(),
-                                 cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    print(t.is_alive())
+        if cnts and len(threadList) == 0:
+            print("TS")
+            t = threading.Thread(target=OHOHOHOHO)
+            threadList.append(t)
+            threadList[0].start()
 
-    if cnts and not t.is_alive():
-        print("WHYEN")
-        time.sleep(1)
-        t.start()
+        cv2.imshow('Capturing', frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
 
-
-    cv2.imshow('Capturing', frame)
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
-
-video.release()
-cv2.destroyAllWindows()
+    video.release()
 
 
+if __name__ == "__main__":
+    main()
--- tt.py
+++ tt.py
@@ -1,36 +1,68 @@
 import threading
 import time
+from playsound import playsound
 
 
-def sleeper():
-    time.sleep(2)
-    print("fff")
+# def sleeper():
+#     time.sleep(2)
+#     print("fff")
 
 
-def main():
-    print("starting")
-    
-    t = threading.Thread(target = sleeper)
-    print("first t is alive: ", t.is_alive())
+# def main():
+#     print("starting")
 
-    t.start()
-    print("t is alive: ", t.is_alive())
-    print("Finish")
-    # t.join()
-    time.sleep(3)
-    print("still alive? ", t.is_alive())
+#     t = threading.Thread(target = sleeper)
+#     print("first t is alive: ", t.is_alive())
+
+#     t.start()
+#     print("t is alive: ", t.is_alive())
+#     print("Finish")
+#     # t.join()
+#     time.sleep(3)
+#     print("still alive? ", t.is_alive())
 
 
+# def tt():
+#     print("YOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYOYO")
+#     print("YOYOYO1")
+#     print("YOYOYO2")
+#     time.sleep(2)
 
-def main2():
-    t = threading.Thread(target = sleeper)
 
-    t.start()
-    t.start()
-    t.start()
-    t.start()
-    t.start()
-    t.start()
+# def main3():
+#     t = threading.Thread(target=tt)
+#     while True:
+#         print("fff")
+#         time.sleep(2)
+#         t.start()
+#         time.sleep(20)
+#         print("start again")
+#         t.start()
 
+
+def main4():
+    string = "96742 + 33130 ="
+    if "+" in string:
+        tmp = string.split("=")[0].replace(" ", "").split("+")
+        print(int(tmp[0])+int(tmp[1]))
+    elif "-" in string:
+        tmp = string.split("-")
+        print(int(tmp[0])-int(tmp[1]))
+    elif "*" in string:
+        tmp = string.split("*")
+        print(int(tmp[0])*int(tmp[1]))
+    elif "%" in string:
+        tmp = string.split("%")
+        print(int(tmp[0]) % int(tmp[1]))
+
+
+# def main2():
+#     t = threading.Thread(target = sleeper)
+#     t.start()
+#     t.start()
+#     t.start()
+#     t.start()
+#     t.start()
+#     t.start()
 if __name__ == ("__main__"):
-    main2()
+    main4()
